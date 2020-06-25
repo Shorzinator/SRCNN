327ef8b5ea788dd8b6a6c0a85976570f32d13b74
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2020-06-25 13:49:23 +0530

    The SRCNN model is defined which is a sequential model 2-layer Convolutional Neural Network

--- sourcecode.py
+++ sourcecode.py
@@ -89,3 +89,27 @@ for file in os.listdir('./SRCNN/images/'):
     ref = cv2.imread('./SRCNN/source/{}'.format(file))
     scores = compare_images(target, ref)
     print('{}\n PSNR: {}\n MSE: {}\n SSIM: {}\n'.format(file, scores[0], scores[1], scores[2]))
+
+
+#define the SRCNN model
+
+def model():
+
+    # define model type
+    SRCNN = Sequential()
+
+    #add model layers
+    SRCNN.add(Conv2D(filters=128, kernel_size = (9,9), kernel_initializer='glorot_uniform',
+             activation='relu', padding='valid', use_bias=True, input_shape=(None, None, 1)))
+    SRCNN.add(Conv2D(filters=64,  kernel_size = (3,3), kernel_initializer='glorot_uniform',
+             activation='relu', padding='same', use_bias=True))
+    SRCNN.add(Conv2D(filters=1,   kernel_size = (5,5), kernel_initializer='glorot_uniform',
+             activation='linear', padding='valid', use_bias=True))
+
+    #define optimizer
+    adam = Adam(lr=0.0001)
+
+    #compile mode
+    SRCNN.compile(optimizer=adam, loss='mean_squared_error', metrics=['mean_squared_error'])
+
+    return SRCNN
